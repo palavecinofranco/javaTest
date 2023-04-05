368d429a3363ec1e78847aeed4b0b1be3d0da9be
palavecinofranco <francopalavecinoagus@gmail.com>
2023-04-04 21:16:07 -0300

    Se agrega toString a rectangulo

--- src/figuras/Rectangulo.java
+++ src/figuras/Rectangulo.java
@@ -12,4 +12,11 @@ public class Rectangulo {
 	public Double getPerimetro() {
 		return this.ancho * 2 + this.alto * 2;
 	}
+
+	@Override
+	public String toString() {
+		return "Rectangulo [ancho=" + ancho + ", alto=" + alto + "]";
+	}
+	
+	
 }
